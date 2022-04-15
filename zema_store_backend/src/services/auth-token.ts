183272d8b942bcc6fadf89c355917e0244b346e1
enochadane <henokadane8@gmail.com>
2022-04-15 23:57:13 +0300

    writting all mongoose schemas

--- zema_store_backend/src/services/auth-token.ts
+++ zema_store_backend/src/services/auth-token.ts
@@ -1,7 +1,6 @@
 import jwt from "jsonwebtoken";
 
 import configs from "../configs/app.configs";
-import User from "../models/user";
 
 export const generateAccessToken = async (payload: any) => {
   const token = jwt.sign(payload, configs.JWT_SECRET, {
